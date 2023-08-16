7eb255769c91a2e145d54e073c792fbb38e3dc91
TheElderLord <61541224+TheElderLord@users.noreply.github.com>
2023-08-16 18:52:49 +0500

    Admin page

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -9,40 +9,50 @@
 
         </div>
         <header>
-            <h3>Добавить запись</h3>
+            <h3>{{lang === 'rus' ? 'Добавить запись' : 
+                lang === 'kz' ? 'Жазба косу' : 'Add a record'}}</h3>
         </header>
         
         <div class="container">
-            <div class="title"><h3>Запись</h3></div>
+            <div class="title"><h3>{{lang ==='rus' ? 'Запись' : lang === 'kz' ? 'Жазба' : 'Record'}}</h3></div>
             <div class="form">
                 <div>
-                <input type="text" v-model="fullname" name="fullname" placeholder="Ф.И.О" required>
-                <input type="text" v-model="iin" name="iin" placeholder="ИИН" required>
-                <input type="text" v-model="birthday" name="birthday" placeholder="День рождения" required>
-                <input type="text" v-model="organ_title" name="organ_title" placeholder="Организация" required>
-                <input type="text" v-model="studying_period" name="studying_period" placeholder="Период обучения" required>
-                <input type="text" v-model="type" name="type" placeholder="Формат обучения" required>
-                <input type="text" v-model="serialNumber" name="serialNumber" placeholder="Серийный номер" required>
-                <input type="text" v-model="number" name="number" placeholder="Номер диплома" required>
-                <div class="warning" v-if="warning">Заполните все поля</div>
+                    <input type="text" v-model="fullname" name="fullname" :placeholder="lang === 'rus' ? 'Ф.И.О' : lang === 'kz' ? 'Аты-жөні' : 'Full Name'" required>
+                    <input type="text" v-model="iin" name="iin" :placeholder="lang === 'rus' ? 'ИИН' : lang === 'kz' ? 'ЖСН' : 'ID Number'" required>
+                    <input type="text" v-model="birthday" name="birthday" :placeholder="lang === 'rus' ? 'День рождения' : lang === 'kz' ? 'Туған күні' : 'Date of Birth'" required>
+                    <input type="text" v-model="organ_title" name="organ_title" :placeholder="lang === 'rus' ? 'Организация' : lang === 'kz' ? 'Ұйым' : 'Organization'" required>
+                    <input type="text" v-model="studying_period" name="studying_period" :placeholder="lang === 'rus' ? 'Период обучения' : lang === 'kz' ? 'Оқу периоды' : 'Study Period'" required>
+                    <input type="text" v-model="type" name="type" :placeholder="lang === 'rus' ? 'Формат обучения' : lang === 'kz' ? 'Оқу форматы' : 'Study Format'" required>
+                    <input type="text" v-model="serialNumber" name="serialNumber" :placeholder="lang === 'rus' ? 'Серийный номер' : lang === 'kz' ? 'Сериялық нөмір' : 'Serial Number'" required>
+                    <input type="text" v-model="number" name="number" :placeholder="lang === 'rus' ? 'Номер диплома' : lang === 'kz' ? 'Диплом нөмірі' : 'Diploma Number'" required>
+                    <div class="warning" v-if="warning">{{ lang === 'rus' ? 'Заполните все поля' : lang === 'kz' ? 'Барлық жолдарды толтырыңыз' : 'Please fill in all fields' }}</div>
 
-                <button @click="addRecord()" >Добавить</button>
-                <button @click="pops()" v-if="img_link">Создать QR</button>
+
+                    <button @click="addRecord()">
+                        {{ lang === 'rus' ? 'Добавить' : lang === 'kz' ? 'Қосу' : 'Add' }}
+                      </button>
+                      <button @click="pops()" v-if="img_link">
+                        {{ lang === 'rus' ? 'Создать QR' : lang === 'kz' ? 'QR жасау' : 'Generate QR' }}
+                      </button>
                
             </div>
                 
             </div>
             <div class="qrimage" v-if="pop_up">
-                <div class="title"><h3>QR </h3></div>
+                <div class="title"><h3>{{ lang === 'rus' ? 'QR' : lang === 'kz' ? 'QR' : 'QR' }}</h3></div>
                 <div class="imageBlock">
-                    <img  :src="`${img_link}`" alt="Fetched Image">
-                    
-                  </div>
-                  <div class="butt">
-                    <a :href="`${img_link}`" download="image.jpg">Скачать</a>
-                  <button @click="pops()" >Закрыть</button>
+                  <img :src="`${img_link}`" alt="Fetched Image">
                 </div>
-            </div>
+                <div class="butt">
+                  <a :href="`${img_link}`" download="image.jpg">
+                    {{ lang === 'rus' ? 'Скачать' : lang === 'kz' ? 'Жүктеу' : 'Download' }}
+                  </a>
+                  <button @click="pops()">
+                    {{ lang === 'rus' ? 'Закрыть' : lang === 'kz' ? 'Жабу' : 'Close' }}
+                  </button>
+                </div>
+              </div>
+              
 
             <div class="cards">
             <div class="card">
