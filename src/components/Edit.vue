1c5e69640ab5591634d35d69a6c5a3758d9d9f3e
TheElderLord <61541224+TheElderLord@users.noreply.github.com>
2023-08-21 13:17:35 +0600

    Name errors

--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -12,7 +12,7 @@ export default {
       serialNumber: "",
       number: "",
       img_link: "",
-      speiality: "",
+      speciality: "",
     }
   },
   methods: {
@@ -38,7 +38,7 @@ export default {
       this.type = d.study_type;
       this.serialNumber = d.serial_number;
       this.number = d.diplom_number;
-      this.speiality = d.specialty;
+      this.speciality = d.speciality;
       
 
     },
@@ -66,7 +66,7 @@ export default {
           serialNumber: this.serialNumber,
           number: this.number,
           lang: this.lang,
-          specialty: this.specialty,
+          speciality: this.speciality,
         }),
       });
       const data = await response.message;
@@ -110,7 +110,7 @@ export default {
             required>
           <input type="text" v-model="number" name="number"
             :placeholder="lang === 'rus' ? 'Номер диплома' : lang === 'kz' ? 'Диплом нөмірі' : 'Diploma Number'" required>
-            <input type="text" v-model="speiality" name="speciality" :placeholder="lang === 'rus' ? 'Специальность' : lang==='kz' ? 'Мамандық' : 'Speciality'">
+            <input type="text" v-model="speciality" name="speciality" :placeholder="lang === 'rus' ? 'Специальность' : lang==='kz' ? 'Мамандық' : 'Speciality'" required>
                    
           <div class="warning" v-if="warning">{{ lang === 'rus' ? 'Заполните все поля' : lang === 'kz' ? 'Барлық жолдарды толтырыңыз' : 'Please fill in all fields' }}</div>
 
